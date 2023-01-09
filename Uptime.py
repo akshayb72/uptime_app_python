310c5c47415b9768a49d66a28d921fc0a78bed14
Akshay B <77104366+akshayb72@users.noreply.github.com>
2023-01-09 22:06:42 +0530

    Update Uptime.py

--- Uptime.py
+++ Uptime.py
@@ -3,13 +3,9 @@ import
 
 url = input("Enter the URL to be checked in the format https://www.examplewebsite.com or http://www.examplewebsite.com \n")
 
-
-
 while True:
-
     res =req.get(url)
-
-    print(res)
+    
     if(res.status_code == 200):
         print("Site is UP")
         print(f"The Response code is {res.status_code}")
